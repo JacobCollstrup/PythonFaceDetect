56adcaecd8fb6b6235b635f01b3b83bd80824e18
Jacob Collstrup <jacob@collstrup.dk>
2021-08-03 21:38:33 +0200

    Target priority finished.

--- FaceDetect.py
+++ FaceDetect.py
@@ -22,21 +22,38 @@ faces = faceCascade.detectMultiScale2(
 
 
 # attempting target priority:
-priority = []
-targets = []
+priority = [] # list for holding confidence values
+targets = [] # list for holding the targets that pass the confidence threshold
+
+# Finding candidates that meets confidence threshold
 for i in range(0, len(faces[1])):
     if faces[1][i][0] > 75:
         priority.append(faces[0][i][2] * faces[0][i][3] * faces[1][i][0])
         targets.append(faces[0][i])
+
+ # Finding the largest target box, ie the closest target due to perspective geometry
+closestTarget = max(priority)
+targetNumber = priority.index(closestTarget)
+
+target = faces[0][targetNumber] # Actual target to pass on from aimbot.
 
 for j in range(0, len(priority)):
     print(f"Target priority: {priority[j]}, Target confidence: {faces[1][j][0]}")
 
 print(f"Found {len(targets)} targets!")
 
+print(target)
+
+# Prepping to draw target box:
+upperLeft = (target[0], target[1]) # Upper left corner of target box
+lowerRight = (target[0]+target[2], target[1]+target[3]) # lower right corner of target box.
+
+cv2.rectangle(image, upperLeft, lowerRight, (0, 255, 0), 2)
+
 # Draw a rectangle around the faces
-for (x, y, w, h) in targets:
-    cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+# for (x, y, w, h) in target:
+#     cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
 
 cv2.imshow("Faces found", image)
 cv2.waitKey(0)
